b895699a030cfd0f098d4cea621de2a899b14e2b
Dmitrii Nasedkin <2_tone@mail.ru>
2021-06-16 22:17:20 +0300

    created tests for junit5

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,65 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashbackHackServiceTest {
+
+    @Test
+    void shouldReturn0IfAmountIs1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    void shouldReturn100IfAmountIs900() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldReturn500IfAmountIs1500() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1500;
+
+        int actual = service.remain(amount);
+        int expected = 500;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldReturn1500IfAmountIsMinus500() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = -500;
+
+        int actual = service.remain(amount);
+        int expected = 1500;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldReturn1000IfAmountIs0() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+
+        int actual = service.remain(amount);
+        int expected = 1000;
+
+        assertEquals(expected, actual);
+    }
+
+}
